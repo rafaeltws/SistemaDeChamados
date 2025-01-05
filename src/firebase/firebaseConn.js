8ab67c38673ab35531242c46a3adf7aeb847a6c0
rafaeltws <159142263+rafaeltws@users.noreply.github.com>
2025-01-05 01:20:53 -0300

    Update firebaseConn.js

--- src/firebase/firebaseConn.js
+++ src/firebase/firebaseConn.js
@@ -3,13 +3,13 @@ import { getAuth } from 'firebase/auth'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBJFFahnjqVtJsR7rTdRzQHafRXnmlcHWI",
-    authDomain: "sistemadechamados-4aa34.firebaseapp.com",
-    projectId: "sistemadechamados-4aa34",
-    storageBucket: "sistemadechamados-4aa34.firebasestorage.app",
-    messagingSenderId: "520426278948",
-    appId: "1:520426278948:web:40d8602c15947ee816c8b1",
-    measurementId: "G-JRGLZJ847S"
+    apiKey: "//",
+    authDomain: "//",
+    projectId: "//",
+    storageBucket: "//",
+    messagingSenderId: "//",
+    appId: "//",
+    measurementId: "//"
   };
 
 const firebaseApp = initializeApp(firebaseConfig);
